de5e52598644c25a6b7e5f8dc25e185e5a57f6a4
RajaUmerSaleem <umerraja0303450@gmail.com>
2025-04-11 20:05:50 +0500

    Initial commit from Create Next App

---
+++ app/page.js
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className=" w-full h-screen flex flex-col items-center justify-center bg-yellow-100">
+    </div>
+  );
+}
